7273add635d4c234596c7ca914373052a313e46e
dogukancaner <dogukanncanerr@gmail.com>
2023-01-17 20:46:34 +0300

    added user login

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+type SearchProps = {
+  loadUser: (username: string) => Promise<void>;
+};
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const Search = () => {
+const Search = ({ loadUser }: SearchProps) => {
+  const [userName, setUserName] = useState("");
+
   return (
-    <div className="flex gap-2">
-      <input type="text" placeholder="Kullanıcı Adı Gir" />
-      <button>
-        <BsSearch />
-      </button>
+    <div className="flex flex-col items-center justify-center gap-4">
+      <h1>Kullanıcı Adını Gir:</h1>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          placeholder="Kullanıcı Adı Gir"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch size={22} />
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,26 @@ import Search from "../components/Search";
 import { UserProps } from "../types/user";
 
 const Home = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (username: string) => {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    const { avatar_url, login, location, followers, following } = data;
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+    setUser(userData);
+  };
+
   return (
-    <div>
-      <Search />
+    <div className="">
+      <Search loadUser={loadUser} />
+      {user && <p className="text-center mt-4">{user.login}</p>}
     </div>
   );
 };
